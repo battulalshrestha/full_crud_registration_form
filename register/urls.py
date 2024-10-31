faf95232d3905882a35fdc88b9199a5578c9d351
Nishan shrestha <nishanshrestha@gmail.com>
2024-10-31 08:58:13 -0700

    crud in form

--- register/urls.py
+++ register/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
-from .views import home
+from .views import home,show_data,delete_data,edit_data
 urlpatterns = [
-    path('',home,name = "home")
+    path('',home,name = "home"),
+    path('show_data/',show_data,name = "show_data"),
+    path('delete_data/<int:id>/',delete_data,name = "delete_data"),
+    path('edit/<int:id>/', edit_data, name='edit_data'),
+
 ]
--- register/views.py
+++ register/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render,redirect
+from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Register
 from django.contrib import messages
 # Create your views here.
@@ -17,3 +19,34 @@ def home(request):
         user.save()
         messages.success("your form has been submitted successfully")
     return render(request,'register.html')
+def show_data(request):
+    datas = Register.objects.all()
+    context = {'value':datas}
+    
+    return render(request,'show_data.html',context)
+
+def delete_data(request,id):
+    data = Register.objects.get(id= id)
+    data.delete()
+    return redirect('show_data')
+def edit_data(request,id):
+     item = get_object_or_404(Register, id=id)
+
+     if request.method == "POST":
+        # Get updated data from the form
+        item.name = request.POST.get('name')
+        item.email = request.POST.get('email')
+        item.password = request.POST.get('password')
+        item.confirm_password = request.POST.get('confirm_password')
+
+        # Perform basic validation
+        if item.password == item.confirm_password:
+            item.save()  # Save the changes
+            return redirect('show_data')  # Redirect after editing
+        else:
+            # Add an error message if passwords do not match
+            error_message = "Passwords do not match."
+            return render(request, 'edit_template.html', {'item': item, 'error_message': error_message})
+
+    # Render the form with existing item data if GET request
+     return render(request, 'edit_template.html', {'item': item})
